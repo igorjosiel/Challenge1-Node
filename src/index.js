2357857896680e07317340f2405230b6408d4266
Igor Josiel <igorjosielvitalino@gmail.com>
2022-11-08 17:20:13 -0300

    feat: Rotas para cadastrar um novo usuário e buscar por todos estão feitas.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  if (!name || !username) {
+    return response.json({ message: 'Você precisa enviar os dados para o cadastro!', error: true });
+  }
+  
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  });
+
+  return response.json({ message: 'Usuário cadastrado com sucesso!', error: false });
+});
+
+app.get('/users', (request, response) => {
+  return response.json({ data: users, error: false });
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
