25ba191349b4068817f16c0ca7b4603e8e867ef5
Igor Josiel <igorjosielvitalino@gmail.com>
2022-11-08 17:53:54 -0300

    feat: rota que lista todas as tarefas do usuário finalizada.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) return response.status(400).json({ message: 'Usuário não encontrado!', error: true });
+
+  request.user = user;
+  
+  next();
 }
 
 app.post('/users', (request, response) => {
@@ -35,7 +43,9 @@ app.get('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.status(200).json({ data: user.todos, error: false});
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
